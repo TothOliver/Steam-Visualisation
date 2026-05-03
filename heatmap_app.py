fe10c189f60bd6ff572e0c6719a5663c1c0a4510
Oliver Toth <olivertoth.in@gmail.com>
2026-05-03 21:25:03 +0200

    dropdown for heatmap

--- heatmap_app.py
+++ heatmap_app.py
@@ -1,4 +1,6 @@
-from dash import Dash, dcc, html
+import webbrowser
+from threading import Timer
+from dash import Dash, dcc, html, Input, Output
 
 from load_dataset import load_steam_dataset
 from heatmap_data import prepare_heatmap_data
@@ -9,24 +11,55 @@ def create_app():
     df = load_steam_dataset(file_path)
     
     heatmap_data = prepare_heatmap_data(df, top_n=30)
-
-    fig1 = create_tag_heatmap(
-        heatmap_data["default_heatmap_matrix"], 
-        heatmap_data["default_percentage_matrix"]
-    )
     
     app = Dash(__name__)
 
     app.layout = html.Div(
         children=[
             html.H1("Stream Tag Trends"),
-            dcc.Graph(id="tag-heatmap", figure=fig1)
+            html.Label("Select Steam tags:"),
+            dcc.Dropdown(
+                id="tag-selector",
+                options=[
+                    {"label": tag, "value": tag}
+                    for tag in heatmap_data["all_tags_sorted"]
+                ],
+                value=heatmap_data["default_tags"],
+                multi=True,
+                placeholder="Select tags to display..."
+            ),
+            dcc.Graph(id="tag-heatmap")
         ]
     )
 
+    @app.callback(
+        Output("tag-heatmap", "figure"),
+        Input("tag-selector", "value")
+    )
+
+    def update_heatmap(selected_tags):
+        if not selected_tags:
+            selected_tags = heatmap_data["default_tags"]
+
+        selected_tags = [
+            tag for tag in selected_tags
+            if tag in heatmap_data["full_heatmap_matrix"].columns
+        ]
+
+        filtered_heatmap_matrix = heatmap_data["full_heatmap_matrix"][selected_tags]
+        filtered_percentage_matrix = heatmap_data["full_percentage_matrix"][selected_tags]
+
+        fig = create_tag_heatmap(
+            filtered_heatmap_matrix,
+            filtered_percentage_matrix
+        )
+
+        return fig
+    
     return app
 
 
 if __name__ == "__main__":
     app = create_app()
+    Timer(1, lambda: webbrowser.open("http://127.0.0.1:8050/")).start()
     app.run(debug=True)
